d8fd892fe651784568fd93d250703a2c776e538e
Andre Pires <erdnapfaserip@gmail.com>
2025-10-14 14:43:38 -0300

    Challenge 3

--- myApp.js
+++ myApp.js
@@ -7,5 +7,11 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
   });
 
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
 
  module.exports = app;
