7d72568e6842d6280c6197ebf248695a79dfacaf
Andre Pires <erdnapfaserip@gmail.com>
2025-10-14 16:00:56 -0300

    Challenge 6

--- myApp.js
+++ myApp.js
@@ -5,6 +5,13 @@ let app = express();
 
 console.log("Hello World");
 
+// GET - LOGGER
+app.get((req, res, next) => {
+    let string = req.method + " " + req.path + " " + req.ip;
+    console.log(string);
+    next();
+});
+
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
 
